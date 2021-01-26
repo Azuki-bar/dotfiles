5614526e5837d77068cac50f5c434c44da648878
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-01-26 23:38:41 +0900

    プロキシの設定を自動化するスクリプトを書きました。

---
+++ scripts/switch_proxy.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+file=/etc/apt/apt.conf
+proxy_address="http://proxy.uec.ac.jp:8080"
+grep -e '^\#' $file >/dev/null
+if [ $? -ne 0 ];then
+    sudo sed -i -e 's/\(.*\)/\#\1/g' $file 
+    export HTTP_PROXY=
+    export HTTPS_PROXY=
+    export http_proxy=
+    export https_proxy=
+    git config --global --unset http.proxy 
+    git config --global --unset https.proxy 
+    git config --global --unset url."https://github.com/".insteadOf git@github.com:
+    git config --global --unset url."https://".insteadOf git://
+    echo proxy on to off
+else
+    sudo sed -i -e 's/^\#\(.*\)/\1/g' $file 
+    export HTTP_PROXY=$proxy_address
+    export HTTPS_PROXY=$proxy_address
+    export http_proxy=$proxy_address
+    export https_proxy=$proxy_address
+    git config --global http.proxy $proxy_address 
+    git config --global https.proxy $proxy_address 
+    git config --global url."https://".insteadOf git://
+    git config --global url."https://github.com/".insteadOf git@github.com:
+    echo proxy off to on
+fi

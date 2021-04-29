95f816f274f42f477791572103e1bd33d95db2a4
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-04-29 20:52:38 +0900

    remove device option from xkeysnail  config file

--- xkeysnail/start.sh
+++ xkeysnail/start.sh
@@ -6,5 +6,6 @@ killall -q xkeysnail
 
 # xhost +si:localuser:xkeysnail
 xhost +si:localuser:azukibar
-DISPLAY=$DISPLAY /usr/bin/xkeysnail -q $HOME/.config/xkeysnail/config.py --devices "Topre Corporation HHKB Professional" "AT Translated Set 2 keyboard" &
+# DISPLAY=$DISPLAY /usr/bin/xkeysnail -q $HOME/.config/xkeysnail/config.py --devices "Topre Corporation HHKB Professional" "AT Translated Set 2 keyboard" &
+DISPLAY=$DISPLAY /usr/bin/xkeysnail -q $HOME/.config/xkeysnail/config.py &
 #sudo -u xkeysnail DISPLAY=$DISPLAY /usr/local/bin/xkeysnail -q $HOME/.config/xkeysnail/config.py &

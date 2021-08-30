b06b4743a4f4f7acf96bf76b755e3807e643ad8a
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-08-30 20:02:50 +0900

    fix install script

--- vim/link_conf.sh
+++ vim/link_conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p ~/.config/nvim/
-
-for i in `ls |grep -v '.sh$'`; do
+cd ~/dotfiles/
+for i in `ls vim|grep -v '.sh$'`; do
     ln -s `pwd`"/"$i $HOME"/.config/nvim/"$i
 done
 

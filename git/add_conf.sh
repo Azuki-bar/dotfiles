37d02c40209b52e93216585f6a5500eb7be9384d
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-09-24 13:58:10 +0900

    append git config

--- git/add_conf.sh
+++ git/add_conf.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+git config --global user.name Azuki-bar
+git config --global user.email 42642269+Azuki-bar@users.noreply.github.com
+
 git config --global core.quatepath false
 git config --global core.editor nvim
+git config --global core.excludesfile ~/.gitignore_global
 
+git config --global commit.gpgsign true
+
+git config --global pull.rebase false

258b9d7edd12d56327ae7491fbd970aabf3fbe6a
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-01-15 15:13:19 +0900

    add linker

--- vim/link_conf.sh
+++ vim/link_conf.sh
@@ -6,3 +6,5 @@ for i in `ls |grep -v '.sh$'`; do
     ln -s `pwd`"/"$i $HOME"/.config/nvim/"$i
 done
 
+mkdir -p ~/.config/coc/extensions/
+ln -s `pwd`"/coc/package.json" "$HOME/.config/coc/extensions/"

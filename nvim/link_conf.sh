cf8836d439af88a80a9c8bd9fee5eb4523ff9cb8
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-02-11 15:34:55 +0900

    update neovim linker

--- nvim/link_conf.sh
+++ nvim/link_conf.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#mkdir -p ~/.config/nvim/
+mkdir $HOME/.config
+touch $HOME/.config/tmp
 cd ~/dotfiles/nvim
 ln -s `pwd` "$HOME/.config"
+rm $HOME/.config/tmp
 
 #for i in `ls |grep -v '.sh$'`; do
 #    ln -s `pwd`"/"$i $HOME"/.config/nvim/"$i
